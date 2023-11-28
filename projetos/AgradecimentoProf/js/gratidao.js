dc85c3486480f108f1a5d4fb288c715f5d9ea6a0
Amanda Porto <amandaporto_93@hotmail.com>
2023-11-28 19:49:08 -0300

    Nove projeto

---
+++ projetos/AgradecimentoProf/js/gratidao.js
@@ -0,0 +1,9 @@
+function mostrarMensagemFinal() {
+    document.getElementById("container").style.display = "none";
+    document.getElementById("thank-you-text").style.display = "block";
+  }
+
+  function esconderMensagemFinal() {
+    document.getElementById("container").style.display = "block";
+    document.getElementById("thank-you-text").style.display = "none";
+  }
